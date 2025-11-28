9b8d3b7bb4041c9ec255a01a5475e153c0802300
Ijji <ijjibaba4@gmail.com>
2025-11-29 03:29:49 +0500

    Update Bitoperations.java

--- Bitoperations.java
+++ Bitoperations.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Bitoperations {
+public class Bitoperationspractice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -25,4 +25,5 @@ public class Bitoperations {
         
         sc.close();
     }
+
 }
